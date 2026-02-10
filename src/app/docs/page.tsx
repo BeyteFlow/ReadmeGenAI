52a03158768da2d66aae408e4faf8a24f5b60312
Naheel Muhammed <naheelmuhammedpk@gmail.com>
2026-02-10 14:28:46 +0530

    Update page.tsx

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -8,8 +8,7 @@ import { navLinks } from '@/constants/navLinks';
 export default function DocsPage() {
   return (
     <div className="min-h-screen bg-black text-white relative">
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-size[40px_40px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] pointer-events-none" />
-
+ <div className="absolute inset-0 bg-[linear-gradient(to_right,`#80808012_1px`,transparent_1px),linear-gradient(to_bottom,`#80808012_1px`,transparent_1px)] bg-size-[40px_40px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,`#000_70`%,transparent_100%)] pointer-events-none" />
       <Navbar links={navLinks} />
       
       <main className="relative z-10 pt-32 pb-20">
